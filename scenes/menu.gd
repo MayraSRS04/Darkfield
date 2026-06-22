106fe4c9c67d32651905343b4bb539dcc58616b0
MayraSRS04 <mayrarosas1@upb.edu>
2026-06-21 22:16:05 -0400

    mapa con muros y render con tilemaplayer

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,32 +2,4 @@ extends Control
 
 
 func _ready() -> void:
-	print("--- prueba de mapa ---")
-
-	var layout := [
-		"##########",
-		"#........#",
-		"#.##..##.#",
-		"#.#....#.#",
-		"#....##..#",
-		"#.#....#.#",
-		"#.##..##.#",
-		"#........#",
-		"##########",
-	]
-
-	var m := Mapa.new()
-	m.cargar(layout)
-
-	print("dimensiones: ", m.filas, " x ", m.columnas)
-	m.imprimir()
-
-	print("caminables totales: ", m.celdas_caminables().size())
-	print("paredes totales: ", m.celdas_pared().size())
-
-	print("es_caminable(1,1) [zona]: ", m.es_caminable(1, 1))
-	print("es_caminable(0,0) [borde]: ", m.es_caminable(0, 0))
-	print("es_caminable(2,2) [muro interno]: ", m.es_caminable(2, 2))
-	print("es_caminable(-1,5) [fuera]: ", m.es_caminable(-1, 5))
-
-	print("--- fin de prueba ---")
+	print("Menu cargado")
