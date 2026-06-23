976195b9a00635a4884c81740a645e54db41ac65
Paul Timothy Kuno <juchpaul20@gmail.com>
2026-06-23 14:07:49 -0400

    feat: movimiento celda a celda con snap suave para el jugador

--- actors/jugador.gd
+++ actors/jugador.gd
@@ -3,11 +3,39 @@ extends CharacterBody2D
 signal solicito_revelar
 signal solicito_abanderar
 
-func _physics_process(_delta: float) -> void:
-	var direccion := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direccion * GameManager.VELOCIDAD_JUGADOR
-	move_and_slide()
+var destino: Vector2
+var moviendose := false
 
+func _ready() -> void:
+	destino = global_position
+
+func _physics_process(delta: float) -> void:
+	if moviendose:
+		global_position = global_position.move_toward(destino, GameManager.VELOCIDAD_JUGADOR * delta)
+		if global_position.distance_to(destino) < 1.0:
+			global_position = destino
+			moviendose = false
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return
+	var direccion := Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		direccion = Vector2.LEFT
+	elif Input.is_action_pressed("ui_right"):
+		direccion = Vector2.RIGHT
+	elif Input.is_action_pressed("ui_up"):
+		direccion = Vector2.UP
+	elif Input.is_action_pressed("ui_down"):
+		direccion = Vector2.DOWN
+	if direccion != Vector2.ZERO:
+		var celda_actual: Vector2i = get_parent().get_node("Suelo").local_to_map(global_position)
+		var celda_destino: Vector2i = celda_actual + Vector2i(direccion)
+		var pos_destino: Vector2 = get_parent().get_node("Suelo").map_to_local(celda_destino)
+		if not get_parent().get_node("Paredes").get_cell_source_id(celda_destino) != -1:
+			destino = pos_destino
+			moviendose = true
+	velocity = Vector2.ZERO
+	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and not event.echo:
